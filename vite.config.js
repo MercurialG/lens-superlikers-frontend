e24da655052aea1ba02c11c95624f45a28ba9e07
Andres Hortua <fandresh7@gmail.com>
2025-03-06 18:35:05 -0500

    Update Vite configuration to enable HTTPS only in non-production modes

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite'
 import fs from 'fs'
 import path from 'path'
 
-export default defineConfig(({ command }) => ({
+export default defineConfig(({ command, mode }) => ({
   server: {
     headers: {
       'Cross-Origin-Embedder-Policy': 'require-corp',
       'Cross-Origin-Opener-Policy': 'same-origin'
     },
-    https: command === 'serve'
+    https: command === 'serve' && mode !== 'production'
       ? {
           key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
           cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem'))
